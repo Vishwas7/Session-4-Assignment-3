f5e13443815468d0a97f441c16fd9a85c65454f2
Vishwas7 <svishwas788@gmail.com>
2017-06-15 11:55:17 +0530

    Session4-Assignment-3

---
+++ src/Assignment/PatternX.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+public class PatternX {
+
+	public static void main(String[] args) {
+		int size=5;
+	
+		
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				if (row == col || row + col == size - 1) {
+					System.out.print("__");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+
+		}
+
+	}
+}
+
+
